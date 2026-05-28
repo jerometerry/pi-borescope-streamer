2d05100f2477c312dd6e8a4a6c133615d49e79cf
Jerome Terry <jerome.l.terry@gmail.com>
2026-05-28 13:41:45 -0230

    code cleanup

--- src/usb_camera.hpp
+++ src/usb_camera.hpp
@@ -23,7 +23,9 @@ public:
     int readFrame(std::vector<uint8_t> &frameBuffer);
 
 private:
-    static constexpr std::pair<uint16_t, uint16_t> VENDOR_PRODUCT_ID_LIST[] = {{0x2ce3, 0x3828}, {0x0329, 0x2022}};
+    static constexpr std::pair<uint16_t, uint16_t> VENDOR_PRODUCT_ID_LIST[] = 
+        {{0x2ce3, 0x3828}, {0x0329, 0x2022}};
+
     static constexpr int INTERFACE_A_NUMBER = 0;
     static constexpr int INTERFACE_B_NUMBER = 1;
     static constexpr int INTERFACE_B_ALTERNATE_SETTING = 1;
@@ -32,10 +34,9 @@ private:
     static constexpr unsigned int USB_TIMEOUT = 1000;
 
     const DeviceInfo& target;
+
     libusb_context *context{nullptr};
     libusb_device_handle *deviceHandle{nullptr};
-
-    
 
     int read(unsigned char endpoint, std::vector<uint8_t> &buffer, size_t maxSize);
     int write(unsigned char endpoint, const uint8_t* buffer, size_t length);
