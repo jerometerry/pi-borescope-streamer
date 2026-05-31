6755fa540b2d74e7b04a0f3ce5807e464e3d255c
Jerome Terry <jerome.l.terry@gmail.com>
2026-05-31 12:04:07 -0230

    Updating docs

--- include/chunk_metadata.hpp
+++ include/chunk_metadata.hpp
@@ -17,7 +17,14 @@ struct [[gnu::packed]] ChunkMetadata {
     uint8_t cameraNumber;
 
     /**
-     * @brief 
+     * @brief Bit-mask containing 3 values
+     * <p>
+     * Supported features include:
+     * <ul>
+     *   <li>unsigned char hasGravitySensor:1;</li>
+     *   <li>unsigned char buttonPress:1;</li>
+     *   <li>unsigned char otherFlags:6;</li>
+     * </ul>
      */
     uint8_t flags;
 
@@ -26,14 +33,34 @@ struct [[gnu::packed]] ChunkMetadata {
      */
     uint32_t gravitySensor;
 
+    /**
+     * @brief Get hasGravitySensor bit from the flags field
+     */
     bool hasGravitySensor() const { 
         return (flags & 0x01) != 0; 
     }
 
+    /**
+     * @brief Set hasGravitySensor bit in the flags field
+     */
+    void setHasGravitySensor(bool hasGravitySensor) { 
+        if (hasGravitySensor) {
+            flags |= 0x01;
+        } else {
+            flags &= ~0x01;
+        }
+    }
+
+    /**
+     * @brief Get buttonPressed bit from the flags field
+     */
     bool isButtonPressed() const  { 
         return (flags & 0x02) != 0; 
     }
 
+    /**
+     * @brief Set buttonPressed bit in the flags field
+     */
     void setButtonPressed(bool pressed) {
         if (pressed) {
             flags |= 0x02;
@@ -42,10 +69,16 @@ struct [[gnu::packed]] ChunkMetadata {
         }
     }
 
+    /**
+     * @brief Get otherFlags value from the flags field
+     */
     uint8_t getOtherFlags() const { 
         return (flags >> 2) & 0x3F; 
     }
 
+    /**
+     * @brief Set otherFlags value in the flags field
+     */
     void setOtherFlags(uint8_t val) {
         flags &= 0x03;
         flags |= ((val & 0x3F) << 2); 
