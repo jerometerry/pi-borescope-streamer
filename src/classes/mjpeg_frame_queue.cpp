6e9de60e77dd708c89cee11a2e161ef6449a1274
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-09 05:51:28 -0230

    Refactor push method in MjpegFrameQueue
    Refactor frame handling in MjpegFrameQueue to improve mutex locking.

--- src/classes/mjpeg_frame_queue.cpp
+++ src/classes/mjpeg_frame_queue.cpp
@@ -10,17 +10,13 @@ void MjpegFrameQueue::push(BufferPtr frame) {
     if (!frame || frame->empty()) { 
         return;
     }
-
     BufferPtr previousFrame;
-
     {
         MutexLock lock(activeMutex_);
         frameId_++;
-
         // Delay destruction of the previous frame until after activeMutex_ lock is released. 
         // Prevents a potential deadlock, if this is the only remaining reference.
         previousFrame = std::move(frame_);
- 
         frame_ = std::move(frame);
     }
 }
