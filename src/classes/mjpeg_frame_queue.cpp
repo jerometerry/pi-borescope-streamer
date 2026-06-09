5d38165e95936d175c859d12433f6a5036539dfd
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-09 05:49:23 -0230

    Optimize pop method to use move semantics

--- src/classes/mjpeg_frame_queue.cpp
+++ src/classes/mjpeg_frame_queue.cpp
@@ -25,8 +25,8 @@ void MjpegFrameQueue::push(BufferPtr frame) {
     }
 }
 
-BufferPtr MjpegFrameQueue::pop(uint32_t& outFrameId) const {
+BufferPtr MjpegFrameQueue::pop(uint32_t& outFrameId) {
     MutexLock lock(activeMutex_);
     outFrameId = frameId_;
-    return frame_;
+    return std::move(frame_); 
 }
