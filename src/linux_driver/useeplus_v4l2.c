f58fc5832b0ea1aa230cba5eaeee58a9a714ae11
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-12 15:44:18 -0230

    replacing spaces with tabs

--- src/linux_driver/useeplus_v4l2.c
+++ src/linux_driver/useeplus_v4l2.c
@@ -519,9 +519,9 @@ static void useeplus_free_resources(struct usb_useeplus *dev)
 							  dev->urb_buffers[i], dev->urb_dma_addrs[i]);
 			dev->urb_buffers[i] = NULL;
 		}
-		
-        usb_free_urb(dev->urbs[i]);
-        dev->urbs[i] = NULL;
+	
+		usb_free_urb(dev->urbs[i]);
+		dev->urbs[i] = NULL;
 	}
 
 	kfree(dev->parse_buffer);
