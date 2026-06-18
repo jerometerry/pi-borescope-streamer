9754ef062afb9dc707e6ceae0ef66151d9e27152
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-18 20:52:28 -0230

    Adding error handling to response builder

--- user-space/src/classes/zero_allocation_response_builder.cpp
+++ user-space/src/classes/zero_allocation_response_builder.cpp
@@ -7,44 +7,37 @@
 
 #include "video_frame.hpp"
 
-char* writeNewLines(char* position) {
-    constexpr char newLines[4] = {'\r', '\n', '\r', '\n'};
-    char* newPosition = position - 4;
-    std::memcpy(newPosition, newLines, 4);
-    return newPosition;
-}
-
-char* writeLength(char* position, size_t size) {
-    char lb[32];
-    auto [ptr, ec] = std::to_chars(lb, lb + sizeof(lb), size);
-    std::string_view lengthStr(lb, ptr - lb);
-
-    char* newPosition = position - lengthStr.size();
-    std::memcpy(newPosition, lengthStr.data(), lengthStr.size());
-    return newPosition;
-}
-
-char* writePrefix(char* position) {
-    constexpr std::string_view prefix =
-        "--mjpegstream\r\nContent-Type: image/jpeg\r\nContent-Length: ";
-
-    char* newPosition = position - prefix.size();
-    std::memcpy(newPosition, prefix.data(), prefix.size());
-    return newPosition;
-}
-
 std::string_view ZeroAllocationResponseBuilder::build(VideoFrame& frame) {
     size_t size = frame.contentSize();
 
-    char* payloadPtr = reinterpret_cast<char*>(frame.storage.data()) + VideoFrame::PADDING_SIZE;
-    char* startPtr = payloadPtr;
-    char* cursor = startPtr;
+    char lb[32];
+    auto [ptr, ec] = std::to_chars(lb, lb + sizeof(lb), size);
+    size_t lengthStringSize = ptr - lb;
 
-    cursor = writeNewLines(cursor);
-    cursor = writeLength(cursor, size);
-    cursor = writePrefix(cursor);
+    constexpr std::string_view prefix =
+        "--mjpegstream\r\nContent-Type: image/jpeg\r\nContent-Length: ";
+    constexpr size_t newLinesSize = 4;
 
-    size_t totalPayloadSize = (startPtr - cursor) + size;
+    size_t totalHeaderSize = prefix.size() + lengthStringSize + newLinesSize;
+
+    if (totalHeaderSize > VideoFrame::PADDING_SIZE) {
+        throw std::length_error("HTTP Header exceeded reserved padding space");
+    }
+
+    char* start = reinterpret_cast<char*>(frame.storage.data());
+    char* payloadPtr = start + VideoFrame::PADDING_SIZE;
+    char* cursor = payloadPtr;
+
+    cursor -= newLinesSize;
+    std::memcpy(cursor, "\r\n\r\n", newLinesSize);
+
+    cursor -= lengthStringSize;
+    std::memcpy(cursor, lb, lengthStringSize);
+
+    cursor -= prefix.size();
+    std::memcpy(cursor, prefix.data(), prefix.size());
+
+    size_t totalPayloadSize = (payloadPtr - cursor) + size;
 
     return {cursor, totalPayloadSize};
 }
