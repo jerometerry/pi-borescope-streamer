5deeb404220f76f5e872435cdc4df5101b4741a5
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-19 17:12:37 -0230

    cleaning up driver data structure

--- useeplus_v4l2/useeplus_core.h
+++ useeplus_v4l2/useeplus_core.h
@@ -21,7 +21,6 @@
 #define VIDEO_QUEUE_NAME "useeplus-queue"
 #define VIDEO_DEVICE_NAME "useeplus-video"
 
-/* Global Protocol Constant Macros */
 #define NUM_URBS 4
 #define URB_SIZE (16 * 1024)
 #define MAX_FRAME_SIZE (256 * 1024)
@@ -31,7 +30,6 @@
 #define UP_DEF_WIDTH 640
 #define UP_DEF_HEIGHT 480
 
-/* Global Diagnostic String Macro */
 #define DIAG_DATA_FORMAT \
 	"URBs:%lu Err:%lu Pkt:%lu Frm:%lu Deliv:%lu D-SOI:%lu D-EOI:%lu D-Q:%lu Ghost:%lu\n"
 
@@ -43,78 +41,76 @@ enum up_config {
 	USB_TO = 1000,
 };
 
-/**
- * struct up_buffer - Queue wrapper mapping videobuf2 elements to internal lists
- */
 struct up_buffer {
 	struct vb2_v4l2_buffer vb2_buffer;
 	struct list_head list;
 };
 
-/**
- * enum up_stream_state - Multi-threaded operational pipeline bit identifiers
- */
 enum up_stream_state {
 	STREAM_HW_ACTIVE = 0,
 	STREAM_CLIENT_READY = 1,
 };
 
-/**
- * struct up_drv_data - Main centralized hardware controller structure
- */
 struct up_drv_data {
-	struct usb_interface *itf;
-	u8 video_in_ep;
-	u8 video_out_ep;
-	u8 iap_in_ep;
-	u8 iap_out_ep;
+	struct {
+		struct usb_device *udev;
+		struct usb_interface *itf;
+		u8 video_in_ep;
+		u8 video_out_ep;
+		u8 iap_in_ep;
+		u8 iap_out_ep;
+		struct urb *urbs[NUM_URBS];
+		u8 *urb_buffers[NUM_URBS];
+		dma_addr_t urb_dma_addrs[NUM_URBS];
+	} usb;
 
-	// Mutex protecting the video_queue
-	struct mutex v4l2_lock;
-	struct vb2_queue video_queue;
+	struct {
+		struct v4l2_device v4l2_dev;
+		struct video_device video_dev;
 
-	struct usb_device *usb_dev;
-	struct v4l2_device v4l2_dev;
-	struct video_device video_dev;
-	u32 width;
-	u32 height;
+		// Mutex protecting the video_queue
+		struct mutex lock;
+		struct vb2_queue queue;
+		u32 width;
+		u32 height;
+	} v4l2;
 
-	// Spinlock protecting access to ready_queue
-	spinlock_t ready_queue_lock;
-	struct list_head ready_queue;
+	struct {
+		unsigned long flags;
+		// Spinlock protecting access to ready_queue
+		spinlock_t ready_lock;
+		struct list_head ready_queue;
+		u64 sequence;
+	} pipeline;
 
-	u64 sequence;
+	struct {
+		struct workqueue_struct *wq;
+		struct work_struct work;
+		DECLARE_KFIFO_PTR(fifo, u8);
 
-	struct urb *urbs[NUM_URBS];
-	u8 *urb_buffers[NUM_URBS];
-	dma_addr_t urb_dma_addrs[NUM_URBS];
+		u8 *workspace_buf;
+		size_t workspace_len;
 
-	unsigned long streaming;
+		struct up_buffer *active_buf;
+		size_t active_pl_len;
 
-	struct workqueue_struct *wq;
-	struct work_struct work;
+		int frame_id;
+		bool building_frame;
+		bool found_soi;
+		bool eof_reached;
+	} decoder;
 
-	struct up_buffer *active_buf;
-	size_t active_pl_len;
-	int frame_id;
-	bool building_frame;
-	bool found_soi;
-	bool eof_reached;
-
-	DECLARE_KFIFO_PTR(fifo, u8);
-
-	u8 *decode_buf;
-	size_t decode_buf_len;
-
-	unsigned long dbg_urbs_processed;
-	unsigned long dbg_ghost_headers;
-	unsigned long dbg_packets_found;
-	unsigned long dbg_frames_found;
-	unsigned long dbg_frames_dropped_soi;
-	unsigned long dbg_frames_dropped_eoi;
-	unsigned long dbg_frames_dropped_queue;
-	unsigned long dbg_frames_delivered;
-	unsigned long dbg_usb_errors;
+	struct {
+		unsigned long urbs_processed;
+		unsigned long usb_errors;
+		unsigned long packets_found;
+		unsigned long frames_found;
+		unsigned long frames_dropped_soi;
+		unsigned long frames_dropped_eoi;
+		unsigned long frames_dropped_queue;
+		unsigned long frames_delivered;
+		unsigned long ghost_headers;
+	} dbg;
 };
 
 #endif /* _USEEPLUS_CORE_H_ */
