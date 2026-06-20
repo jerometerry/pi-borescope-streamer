acf2135b9e138cbe748b62d49f9d9e514bbc48e5
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-20 07:16:08 -0230

    Converting from CMake to Make

---
+++ mjpeg_streamer/install-deps.sh
@@ -0,0 +1,6 @@
+mkdir -p third_party
+git clone --depth 1 --branch v20.67.0 https://github.com/uNetworking/uWebSockets.git third_party/uWebSockets
+git clone --depth 1 --branch v0.8.8 https://github.com/uNetworking/uSockets.git third_party/uSockets
+cd third_party/uSockets
+make
+cd ../../
