afb1ef858d2c09cf2f96655cc2284ad1b62a2853
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-20 12:58:12 -0230

    Updating install-deps.sh to be rerunnable.

--- mjpeg_streamer/install-deps.sh
+++ mjpeg_streamer/install-deps.sh
@@ -1,7 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
 mkdir -p third_party
-git clone --depth 1 --branch v20.67.0 https://github.com/uNetworking/uWebSockets.git third_party/uWebSockets
-git clone --depth 1 --branch v0.8.8 https://github.com/uNetworking/uSockets.git third_party/uSockets
+
+if [ ! -d "third_party/uWebSockets" ]; then
+    echo "Cloning uWebSockets..."
+    git clone --depth 1 --branch v20.67.0 https://github.com third_party/uWebSockets
+else
+    echo "uWebSockets already exists, skipping clone."
+fi
+
+if [ ! -d "third_party/uSockets" ]; then
+    echo "Cloning uSockets..."
+    git clone --depth 1 --branch v0.8.8 https://github.com third_party/uSockets
+else
+    echo "uSockets already exists, skipping clone."
+fi
+
+echo "Building uSockets with Zig..."
 cd third_party/uSockets
 make clean
 make CC="zig cc" CXX="zig c++" AR="zig ar" WITH_LTO=0
 cd ../../
+
+echo "Dependencies successfully configured!"
