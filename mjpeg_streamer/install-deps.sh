8f3e9b27a90d1ff3c090ac21005f94244556ef99
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-20 12:56:20 -0230

    Fixing zig compilation

--- mjpeg_streamer/install-deps.sh
+++ mjpeg_streamer/install-deps.sh
@@ -3,5 +3,5 @@ git clone --depth 1 --branch v20.67.0 https://github.com/uNetworking/uWebSockets
 git clone --depth 1 --branch v0.8.8 https://github.com/uNetworking/uSockets.git third_party/uSockets
 cd third_party/uSockets
 make clean
-make CC="zig cc" CXX="zig c++" AR="zig ar" CFLAGS="-std=c11 -Isrc -O3 -DLIBUS_NO_SSL"
+make CC="zig cc" CXX="zig c++" AR="zig ar" WITH_LTO=0
 cd ../../
