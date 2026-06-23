f2e8cf422bb9576558e827c2f8e8187d7ce9283d
Jerome Terry <jerome.l.terry@gmail.com>
2026-06-23 20:11:52 -0230

    Moving files to useeplus repo

--- mjpeg_streamer/include/array_buffer.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "array.hpp"
-#include "disruptor.hpp"
-
-inline constexpr int64_t ARRAY_BUFFER_CAPACITY = 128;
-
-using ArrayBuffer =
-    disruptor::Disruptor<Array, ARRAY_BUFFER_CAPACITY, disruptor::BlockingWaitStrategy>;
--- mjpeg_streamer/install-core-deps.sh
+++ mjpeg_streamer/install-core-deps.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-SOCKETS_DIR="build/sockets"
-mkdir -p "$SOCKETS_DIR"
+USEEPLUS_DIR="build/useeplus"
 
-if [ ! -d "$SOCKETS_DIR/uWebSockets" ]; then
-    echo "Cloning uWebSockets..."
-    git clone -c advice.detachedHead=false --depth 1 --branch v20.67.0 https://github.com/uNetworking/uWebSockets.git "$SOCKETS_DIR/uWebSockets"
+if [ ! -d "$USEEPLUS_DIR" ]; then
+    echo "Cloning useeplus..."
+    git clone -c advice.detachedHead=false --branch main https://github.com/jerometerry/useeplus.git "$USEEPLUS_DIR"
 fi
 
-if [ ! -d "$SOCKETS_DIR/uSockets" ]; then
-    echo "Cloning uSockets..."
-    git clone -c advice.detachedHead=false --depth 1 --branch v0.8.8 https://github.com/uNetworking/uSockets.git "$SOCKETS_DIR/uSockets"
-fi
-
-echo "Building uSockets with Zig..."
-cd "$SOCKETS_DIR/uSockets"
+echo "Building useeplus..."
+cd "$USEEPLUS_DIR"
 make clean
-make CC="zig cc" CXX="zig c++" AR="zig ar" WITH_LTO=0
+make
